14cea5d5e3a92bb0da010835190cf5609166c7e9
dna-oliv <62681964+dna-oliv@users.noreply.github.com>
2020-05-11 01:11:33 -0400

    Creates a VPC * Creates a public subnet for each AZ * Creates a private subnet for each AZ

---
+++ high-available-website/main.tf
@@ -0,0 +1,31 @@
+provider "aws" {
+  region = var.region
+}
+
+data "aws_availability_zones" "az" {}
+
+resource "aws_vpc" "VPC" {
+  cidr_block           = "10.0.0.0/16"
+  enable_dns_hostnames = true
+}
+resource "aws_subnet" "public-subnets" {
+  vpc_id = aws_vpc.VPC.id
+  cidr_block = "10.0.${0+count.index}.0/24"
+  map_public_ip_on_launch = true
+  count = length(data.aws_availability_zones.az.names)
+  availability_zone = data.aws_availability_zones.az.names[count.index]
+  tags = {
+      Name = "Public"
+  }
+}
+resource "aws_subnet" "private-subnets" {
+  vpc_id = aws_vpc.VPC.id
+  cidr_block = "10.0.${10+count.index}.0/24"
+  map_public_ip_on_launch = false
+  count = length(data.aws_availability_zones.az.names)
+  availability_zone= data.aws_availability_zones.az.names[count.index]
+
+  tags = {
+      Name = "Private"
+  }
+}
