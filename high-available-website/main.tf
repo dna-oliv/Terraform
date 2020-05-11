d9a24d985cf0884476a5f30ebdc3ba21b7f02576
dna-oliv <62681964+dna-oliv@users.noreply.github.com>
2020-05-11 03:13:19 -0400

    Adds a route to the Default Route table

--- high-available-website/main.tf
+++ high-available-website/main.tf
@@ -13,6 +13,15 @@ resource "aws_internet_gateway" "igw" {
   vpc_id = aws_vpc.vpc.id
 }
 
+resource "aws_default_route_table" "route-table" {
+  default_route_table_id = aws_vpc.vpc.default_route_table_id
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.igw.id
+  }
+}
+
 resource "aws_subnet" "public-subnets" {
   vpc_id = aws_vpc.vpc.id
   cidr_block = "10.0.${0+count.index}.0/24"
