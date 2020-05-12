44b369a08903c68313151a5e9a3d2feb9e08cb63
dna-oliv <62681964+dna-oliv@users.noreply.github.com>
2020-05-12 11:09:17 -0400

    Adds a Security Group to the ALB

--- high-available-website/main.tf
+++ high-available-website/main.tf
@@ -116,3 +116,32 @@ resource "aws_alb" "alb" {
     Name = "ALB NAME"
   }
 }
+
+resource "aws_security_group" "alb-sg" {
+  name        = "alb-sg"
+  description = "Application Load Balancer Security Group"
+  vpc_id      = aws_vpc.vpc.id
+
+  ingress {
+    description = "Allow http access to the ALB"
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    description = "Allow https access to the ALB"
+    from_port   = 443
+    to_port     = 443
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = -1
+    cidr_blocks =  ["0.0.0.0/0"]
+  }
+}
