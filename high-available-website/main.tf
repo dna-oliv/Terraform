46136dcc160d4b57a37ad1e3eabc3b09386b1325
dna-oliv <62681964+dna-oliv@users.noreply.github.com>
2020-05-12 12:33:11 -0400

    Creates an ALB Listener

--- high-available-website/main.tf
+++ high-available-website/main.tf
@@ -163,3 +163,14 @@ resource "aws_alb_target_group" "alb-target" {
     matcher             = 200
   }
 }
+
+resource "aws_alb_listener" "alb-listener" {
+  load_balancer_arn = aws_alb.alb.arn
+  port              = 80
+  protocol          = "HTTP"
+
+  default_action {
+    type = "forward"
+    target_group_arn = aws_alb_target_group.alb-target.arn
+  }
+}
