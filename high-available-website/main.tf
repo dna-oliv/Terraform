addb90dd451abefe8a79f43e77acc7f91f1d1a92
dna-oliv <62681964+dna-oliv@users.noreply.github.com>
2020-05-24 17:33:00 -0400

    Adds Autoscaling Policies to scale up and down

--- high-available-website/main.tf
+++ high-available-website/main.tf
@@ -149,6 +149,22 @@ resource "aws_autoscaling_attachment" "asg-attach" {
   alb_target_group_arn   = aws_alb_target_group.alb-target.arn
 }
 
+resource "aws_autoscaling_policy" "scale-up" {
+  name                   = "Scale Up Policy"
+  scaling_adjustment     = 1
+  adjustment_type        = "ChangeInCapacity"
+  cooldown               = 240
+  autoscaling_group_name = aws_autoscaling_group.autoscaling-group.name
+}
+
+resource "aws_autoscaling_policy" "scale-down" {
+  name                   = "Scale Down Policy"
+  scaling_adjustment     = -1
+  adjustment_type        = "ChangeInCapacity"
+  cooldown               = 240
+  autoscaling_group_name = aws_autoscaling_group.autoscaling-group.name
+}
+
 #----------------------------------------------------------------------------
 # Elastic Load Balancer
 #----------------------------------------------------------------------------
